0b53ff2ac2476ee0696fefb75c8320abd483b806
andretocci <andre.tocci@dp6.com.br>
2021-10-18 09:09:50 -0300

    feat: :rocket: main heuristic model function created

--- marketing_attribution_models/models/main.py
+++ marketing_attribution_models/models/main.py
@@ -1,7 +1,9 @@
+import pandas as pd
+
 from my_model import HMODELS
 
 
-def heurist_models(model_name, channels, conv_value, args=None):
+def heurist_models(model_name, channels, conv_value=None, has_conv=None, args=None):
     """
     Função que carrega um modelo que foi implementado
     e aplica em um pd.Series contendo listas de canais.
@@ -11,8 +13,14 @@ def heurist_models(model_name, channels, conv_value, args=None):
     model_name : str or function
         Model name
     channels : pd.Series
-        pd.Series contendo lista de canais
+        pd.Series containing a list
     conv_value : int or pd.Series
+        If value is a pd.Series, obj must be the
+        same lenght passed on 'channels' innit
+    has_conv : pd.Series
+        If value is a pd.Series, obj must be the
+        same lenght passed on 'channels' innit
+    has_conv : pd.Series
         If value is a pd.Series, obj must be the
         same lenght passed on 'channels' innit
 
@@ -21,14 +29,48 @@ def heurist_models(model_name, channels, conv_value, args=None):
     results : pd.Series
     """
 
+    # Creating the objetc
     my_model = HMODELS(channels)
 
+    # Loading model
+    ## Checking if a custom function was passed
     if callable(model_name):
-        my_model.load_model(model_name)
+        try:
+            my_model.load_custom_model(model_name)
+        except Exception as e:
+            print("Custom function must be applyied to a list and retur a list\n", e)
 
     else:
-        my_model.load_custom_model(model_name)
+        my_model.load_model(model_name)
 
-    my_model.fit(*args)
-    my_model.apply_value(conv_value)
+    # Fitting the model
+    if args is None:
+        my_model.fit()
+    else:
+        my_model.fit(*args)
+
+    # Appling value to the results
+    if conv_value is not None:
+        my_model.apply_value(conv_value)
+
+    # Appling value to the results
+    if has_conv is not None:
+        my_model.apply_value(has_conv)
+
     return my_model.get_df(), my_model.group_results()
+
+
+if __name__ == "__main__":
+    channels = pd.Series([["x", "y", "z"], ["x", "y", "z", "y", "z"], ["z"]])
+    values = pd.Series([1, 7, 22])
+    has_conv = pd.Series([True, True, False])
+
+    model = "last_click_non"
+    custom_param = "z"
+    results = heurist_models(model, channels, values, has_conv, args=custom_param)
+    print(results)
+
+    model = "last_click"
+    custom_param = None
+    results = heurist_models(model, channels, values, has_conv, args=custom_param)
+    print(results)
